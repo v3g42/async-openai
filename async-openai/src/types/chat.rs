2a08710b72ca20bf7cf962f68dea51068598739b
Karolis Gudiškis <karolisgud@gmail.com>
2025-01-10 10:13:21 +0200

    Support eos as finish reason

--- async-openai/src/types/chat.rs
+++ async-openai/src/types/chat.rs
@@ -551,6 +551,7 @@ pub struct ChatCompletionStreamOptions {
 #[derive(Debug, Serialize, Deserialize, Clone, Copy, PartialEq)]
 #[serde(rename_all = "snake_case")]
 pub enum FinishReason {
+    #[serde(alias = "eos")]
     Stop,
     Length,
     ToolCalls,
